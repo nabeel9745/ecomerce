6662ba8ffe3a574194e25729d0ad2dfe4f4bc5a5
nabeel <nabeel4785@gmail.com>
2022-05-14 11:45:40 +0530

    change in

--- common/urls.py
+++ common/urls.py
@@ -4,6 +4,6 @@ from .import views
 app_name='common'
 
 urlpatterns=[
-path('',views.customerregister),
-path('login',views.login),
+path('customerregistration',views.customerregister),
+path('',views.login),
 ]
