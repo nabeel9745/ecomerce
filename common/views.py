4cd5c52702191194ce464d3a8251a0e905d1ad84
nabeel <nabeel4785@gmail.com>
2022-05-14 11:38:47 +0530

    change in template

--- common/views.py
+++ common/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def customerregister(request):
-    return render(request,'customerregister/customer register.html')
+    return render(request,'common/customer register.html')
 
 def login(request):
-    return render(request,'login/login.html')
+    return render(request,'common/login.html')
 
     
