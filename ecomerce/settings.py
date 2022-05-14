271f2f63ac2d120de3fd09c4555b43b958d1759f
nabeel <nabeel4785@gmail.com>
2022-05-14 11:29:28 +0530

    second

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+m7n!sizix4usd3!(k^&+f$_%9=(44n1gl^@nfe!fic3_2!9h1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
